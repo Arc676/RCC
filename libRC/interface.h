a725eb9d0230faed761b635975db011f3e16b58b
Arc676/Alessandro Vinciguerra <alesvinciguerra@gmail.com>
2023-12-20 15:26:35 +0800

    Remove erroneous character

--- libRC/interface.h
+++ libRC/interface.h
@@ -1,7 +1,7 @@
 #ifndef INTERFACE_H
 #define INTERFACE_H
 
-#define DEFAULT_CONTROL_PORT 6728;  // "Open RC Vehicle" on a 10-key layout
+#define DEFAULT_CONTROL_PORT 6728  // "Open RC Vehicle" on a 10-key layout
 
 #define PING         0x01
 #define PING_ACK     0x02
