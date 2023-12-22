925d83c0524b5e4c34dfe72369f0455cd7fd149a
Arc676/Alessandro Vinciguerra <alesvinciguerra@gmail.com>
2023-12-22 20:18:49 +0800

    Implement camera state de/serialization

--- libRC/Peripherals/camera.cpp
+++ libRC/Peripherals/camera.cpp
@@ -1,20 +1,49 @@
 #include "camera.h"
 
 #include <cstddef>
+#include <cstdio>
+#include <cstring>
 #include <memory>
 #include <vector>
 
 #include "libcamera/camera.h"
 
+void CameraState::prepareCameraList(const size_t count) {
+	cameras.reserve(count);
+	cameras.clear();
+}
+
 void CameraState::loadCameraNames(
 	const std::vector<std::shared_ptr<libcamera::Camera>>& cams) {
-	cameras.reserve(cams.size());
-	cameras.clear();
+	prepareCameraList(cams.size());
 	for (const auto& cam : cams) {
 		cameras.push_back(cam->id());
 	}
 }
 
-size_t CameraState::serialize(byte* buf) const {}
+size_t CameraState::serialize(byte* buf) const {
+	size_t written = 0;
+	Metadata meta{(byte)enabled, selectedCam, cameras.size()};
+	memcpy((void*)buf, &meta, sizeof(Metadata));
+	written += sizeof(Metadata);
+	for (const auto& cam : cameras) {
+		size_t len = cam.length();
+		memcpy(buf + written, cam.c_str(), len + 1);
+		written += len + 1;
+	}
+	return written;
+}
 
-void CameraState::deserialize(const byte* buf, const size_t len) {}
+void CameraState::deserialize(const byte* buf, const size_t len) {
+	Metadata meta;
+	memcpy(&meta, (void*)buf, sizeof(Metadata));
+	enabled     = (meta.enabled != 0U);
+	selectedCam = meta.selectedCam;
+	size_t pos  = sizeof(Metadata);
+	prepareCameraList(meta.camCount);
+	for (int i = 0; i < meta.camCount; i++) {
+		cameras.emplace_back((char*)buf + pos);
+		pos += cameras.back().size() + 1;
+	}
+	deserializedSize = len;
+}
--- libRC/Peripherals/camera.h
+++ libRC/Peripherals/camera.h
@@ -8,11 +8,22 @@
 
 #include "interface.h"
 #include "libcamera/camera.h"
+
 struct CameraState {
+	struct __attribute__((packed)) Metadata {
+		byte enabled;
+		unsigned selectedCam;
+		size_t camCount;
+	};
+
 	bool enabled = false;
 
 	unsigned selectedCam = 0;
 	std::vector<std::string> cameras;
+
+	size_t deserializedSize = 0;
+
+	void prepareCameraList(size_t);
 
 	void loadCameraNames(
 		const std::vector<std::shared_ptr<libcamera::Camera>>&);
