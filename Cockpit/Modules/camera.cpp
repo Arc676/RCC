Arc676/RCC

commit 61a788178230b2db94a4ef6bf82e431e12525860

WIP: use Buffer for client side modules

Cockpit/Modules/camera.cpp
@@ -149,10 +149,13 @@ void CameraModule::renderViewfinder() {
 	ImGui::End();
 }
 
-void CameraModule::handleMessage(const byte* const msg, const size_t len) {
-	switch (msg[0]) {
+void CameraModule::handleMessage(ConstBuf& msg) {
+	byte response;
+	msg >> response;
+	switch (response) {
 		case CAM_STATE:
-			state.deserialize(msg + 1, len - 1);
+			msg >> state;
+			// state.deserialize(msg + 1, len - 1);
 			if (state.cameraIsRunning()) {
 				const auto& names = state.getCameraNames();
 				size_t idx        = state.getSelected();
@@ -164,17 +167,16 @@ void CameraModule::handleMessage(const byte* const msg, const size_t len) {
 			}
 			break;
 		case CAM_PROPS:
-			camProps = CameraData::deserialize(msg + 1, len - 1);
+			msg >> camProps;
+			// camProps = CameraData::deserialize(msg + 1, len - 1);
 			break;
 		case CAM_OK:
 			requestCmd(CAM_QUERY);
 			lastResult = CameraState::CAMERA_OK;
 			break;
 		case CAM_ERROR:
-			if (len >= 1 + sizeof(enum CameraState::CameraResult)) {
-				memcpy(&lastResult, msg + 1,
-				       sizeof(enum CameraState::CameraResult));
-			} else {
+			msg >> lastResult;
+			if (!msg.ok()) {
 				lastResult = CameraState::UNKNOWN_ERROR;
 			}
 			break;

Cockpit/Modules/module.h
@@ -8,6 +8,7 @@
 #include <cstring>
 #include <utility>
 
+#include "Stream/buffer.h"
 #include "Stream/netstream.h"
 #include "interface.h"
 
@@ -21,6 +22,8 @@ class Module {
 	const Dashboard* dash;
 
 protected:
+	using ConstBuf = Buffer<const byte>;
+
 	/**
 	 * @brief Get the dashboard object
 	 *
@@ -96,9 +99,8 @@ public:
 	 * @brief Handle an incoming message
 	 *
 	 * @param msg Message buffer
-	 * @param len Message length
 	 */
-	virtual void handleMessage(const byte* msg, size_t len) = 0;
+	virtual void handleMessage(ConstBuf& msg) = 0;
 
 	/**
 	 * @brief Determine whether this module should be used to handle SDL events

Cockpit/Modules/ping.cpp
@@ -36,13 +36,15 @@ const char* PingModule::decodeResponse() const {
 	}
 }
 
-void PingModule::handleMessage(const byte* msg, size_t len) {
-	response = msg[0];
+void PingModule::handleMessage(ConstBuf& msg) {
+	byte response;
+	msg >> response;
+
 	auto now = std::chrono::system_clock::now();
 	latency  = std::chrono::duration_cast<std::chrono::microseconds>(
                   now.time_since_epoch())
 	          - latency;
-	if (len != 1) {
+	if (msg.size() != 1) {
 		response = PING_INVALID;
 		return;
 	}

Cockpit/Modules/ping.h
@@ -28,7 +28,7 @@ public:
 		return opCode == PING_ACK;
 	}
 
-	void handleMessage(const byte* msg, size_t len) override;
+	void handleMessage(ConstBuf&) override;
 
 	void render() override;
 };

Cockpit/Modules/rc.cpp
@@ -48,6 +48,11 @@ void RCModule::render() {
 
 		transmissionControls();
 		renderSocketStates();
+
+		if (!lastReadOK) {
+			ImGui::Text(
+				"Failed to read expected data from last response from vehicle");
+		}
 	}
 }
 
@@ -118,8 +123,11 @@ void RCModule::renderSocketStates() const {
 	}
 }
 
-void RCModule::handleMessage(const byte* const buf, size_t len) {
-	if (buf[0] == RC_OK) {
+void RCModule::handleMessage(ConstBuf& msg) {
+	byte response;
+	msg >> response;
+	lastReadOK = true;
+	if (response == RC_OK) {
 		const auto* last = checkLastCmd().first;
 		switch (last[0]) {
 			case RC_CONFIG: {
@@ -128,21 +136,16 @@ void RCModule::handleMessage(const byte* const buf, size_t len) {
 				break;
 			}
 			case RC_QUERY:
-				if (len >= 1 + sizeof(RCSetup) + sizeof(enum SocketStatus)) {
-					memcpy(&setup, buf + 1, sizeof(RCSetup));
-					memcpy(&remoteSockState, buf + 1 + sizeof(RCSetup),
-					       sizeof(enum SocketStatus));
-				}
+				msg >> setup >> remoteSockState;
+				lastReadOK = msg.ok();
 				break;
 			case RC_STOP:
-				if (len >= 1 + sizeof(enum SocketStatus)) {
-					memcpy(&remoteSockState, buf + 1,
-					       sizeof(enum SocketStatus));
-				}
+				msg >> remoteSockState;
+				lastReadOK = msg.ok();
 				break;
 		}
 	} else {
-		memcpy(&remoteSockState, buf + 1, sizeof(enum SocketStatus));
+		msg >> remoteSockState;
 	}
 }
 

Cockpit/dashboard.cpp
@@ -6,6 +6,7 @@
 #include <cstdio>
 #include <thread>
 
+#include "Stream/buffer.h"
 #include "Stream/netstream.h"
 #include "imgui.h"
 #include "interface.h"
@@ -59,9 +60,10 @@ void Dashboard::handleEvent(const SDL_Event* const event) {
 }
 
 void Dashboard::handleMessage(const byte* msg, size_t len) {
+	Buffer buf(msg, len);
 	for (const auto& module : modules) {
-		if (module->canHandleMessage(msg[0])) {
-			module->handleMessage(msg, len);
+		if (module->canHandleMessage(buf.peek())) {
+			module->handleMessage(buf);
 		}
 	}
 }

Cockpit/Modules/camera.h
@@ -38,7 +38,7 @@ public:
 		return (opCode & OPCODE_ID_BITMASK) == CAMERA_CMD;
 	}
 
-	void handleMessage(const byte*, size_t) override;
+	void handleMessage(ConstBuf&) override;
 };
 
 #endif

Cockpit/Modules/rc.h
@@ -27,6 +27,8 @@ class RCModule : public Module {
 	int transmitDelay = DEFAULT_TRANSMISSION_DELAY;
 	RCSetup setup;
 
+	bool lastReadOK = true;
+
 	enum ControllerType {
 		KEYBOARD,
 		JOYSTICK,
@@ -67,7 +69,7 @@ public:
 
 	void handleEvent(const SDL_Event*) override;
 
-	void handleMessage(const byte*, size_t) override;
+	void handleMessage(ConstBuf&) override;
 
 	void render() override;
 };

commit b039c22b5b3949b5273b83b97899f4be93d4cecd

Use Buffer in Modules
Eliminate raw C arrays from modules
Fix ping module bug introduced in 61a7881

Cockpit/Modules/camera.cpp
@@ -52,10 +52,9 @@ void CameraModule::cameraSelect() {
 			ImGui::EndTable();
 		}
 		if (ImGui::Button("Get camera data")) {
-			byte cmd[1 + sizeof(size_t)] = {CAM_QUERY};
-			size_t selected              = state.getSelected();
-			memcpy(cmd + 1, &selected, sizeof(size_t));
-			requestCmd(cmd, sizeof(cmd));
+			size_t selected = state.getSelected();
+			getCleanCmdBuffer() << CAM_QUERY << selected;
+			requestCmd();
 		}
 		ImGui::TreePop();
 	}

Cockpit/Modules/module.h
@@ -15,8 +15,12 @@
 class Dashboard;
 
 class Module {
+protected:
+	using Buf = Buffer<byte>;
+
+private:
 	bool cmdRequested = false;
-	byte cmd[MESSAGE_BUFLEN];
+	Buf cmd;
 	size_t cmdLength = 0;
 
 	const Dashboard* dash;
@@ -32,34 +36,29 @@ protected:
 	const Dashboard* getDashboard() const { return dash; }
 
 	/**
-	 * @brief Set the command to be sent
+	 * @brief Get the command buffer
 	 *
-	 * @param cmd Command buffer
-	 * @param len Command length
+	 * @return Command buffer
 	 */
-	void setCmd(const byte* cmd, size_t len) {
-		assert(len <= MESSAGE_BUFLEN);
-		memcpy(this->cmd, cmd, len);
-		cmdLength = len;
+	Buf& getCmdBuffer() { return cmd; }
+
+	/**
+	 * @brief Get the command buffer after clearing it
+	 *
+	 * @return Command buffer
+	 */
+	Buf& getCleanCmdBuffer() {
+		cmd.clear();
+		return getCmdBuffer();
 	}
 
 	/**
-	 * @brief Set a single byte command
+	 * @brief Requests a single byte command
 	 *
-	 * @param cmd Command opcode
+	 * @param cmd Command to send
 	 */
-	void setCmd(const byte cmd) {
-		this->cmd[0] = cmd;
-		cmdLength    = 1;
-	}
-
-	void requestCmd(const byte* cmd, size_t len) {
-		setCmd(cmd, len);
-		requestCmd();
-	}
-
 	void requestCmd(const byte cmd) {
-		setCmd(cmd);
+		getCleanCmdBuffer() << cmd;
 		requestCmd();
 	}
 
@@ -67,15 +66,6 @@ protected:
 	 * @brief Indicate that the provided command should be sent to the vehicle
 	 */
 	void requestCmd() { cmdRequested = true; }
-
-	/**
-	 * @brief Check last requested command (for internal use)
-	 *
-	 * @return Buffer and length for last requested command
-	 */
-	std::pair<const byte*, size_t> checkLastCmd() const {
-		return std::make_pair(cmd, cmdLength);
-	}
 
 public:
 	/**
@@ -85,7 +75,8 @@ public:
 	 * information
 	 */
 	Module(const Dashboard* dash)
-		: dash(dash) {}
+		: dash(dash)
+		, cmd(MESSAGE_BUFLEN) {}
 
 	/**
 	 * @brief Determine whether this module is set up to handle a given response
@@ -134,9 +125,9 @@ public:
 	 *
 	 * @return The command buffer and its length as a std::pair
 	 */
-	std::pair<const byte*, size_t> getCmdData() {
+	const Buf& getCmdData() {
 		cmdRequested = false;
-		return std::make_pair(cmd, cmdLength);
+		return cmd;
 	}
 };
 

Cockpit/Modules/ping.cpp
@@ -37,7 +37,6 @@ const char* PingModule::decodeResponse() const {
 }
 
 void PingModule::handleMessage(ConstBuf& msg) {
-	byte response;
 	msg >> response;
 
 	auto now = std::chrono::system_clock::now();

Cockpit/Modules/ping.h
@@ -21,7 +21,7 @@ class PingModule : public Module {
 public:
 	PingModule(const Dashboard* dash)
 		: Module(dash) {
-		setCmd(PING);
+		getCmdBuffer() << PING;
 	}
 
 	bool canHandleMessage(const byte opCode) const override {

Cockpit/Modules/rc.cpp
@@ -48,9 +48,8 @@ void RCModule::streamSetup() {
 		}
 	} else {
 		if (ImGui::Button("Configure Stream")) {
-			static byte buf[1 + sizeof(RCSetup)] = {RC_CONFIG};
-			memcpy(buf + 1, &setup, sizeof(RCSetup));
-			requestCmd(buf, sizeof(buf));
+			getCleanCmdBuffer() << RC_CONFIG << setup;
+			requestCmd();
 		}
 	}
 }
@@ -307,8 +306,9 @@ void RCModule::handleMessage(ConstBuf& msg) {
 	msg >> response;
 	lastReadOK = true;
 	if (response == RC_OK) {
-		const auto* last = checkLastCmd().first;
-		switch (last[0]) {
+		Buf& last = getCmdBuffer();
+		last.rewind();
+		switch (last.peek()) {
 			case RC_CONFIG: {
 				remoteSockState = SOCKET_OK;
 				startTransmitting();

Cockpit/dashboard.cpp
@@ -112,8 +112,8 @@ void Dashboard::commandHandler() {
 	}
 	for (const auto& module : modules) {
 		if (module->shouldSendCmd()) {
-			auto data = module->getCmdData();
-			connection.send(data.first, data.second);
+			const auto& data = module->getCmdData();
+			connection.send(data.getBuffer(), data.size());
 		}
 	}
 }